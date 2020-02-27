1a53aafefdc6d16e39adf5574fd7c3de7265e412
Thies Edeling <thies@getehour.com>
2020-02-27 16:12:06 +0100

    Fix Invalid expires attribute.
    https://stackoverflow.com/questions/36473478/fixing-httpclient-warning-invalid-expires-attribute-using-fluent-api

--- src/main/java/com/getehour/connector/jortt/HttpExecutor.java
+++ src/main/java/com/getehour/connector/jortt/HttpExecutor.java
@@ -5,6 +5,8 @@ import org.apache.http.HttpEntity;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -12,6 +14,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -34,7 +37,7 @@ public class HttpExecutor {
         post.setEntity(input);
 
         try (CloseableHttpClient httpclient = createHttpClient(credentials.getUsername(), credentials.getApiKey());
-            CloseableHttpResponse response = httpclient.execute(post)) {
+             CloseableHttpResponse response = httpclient.execute(post)) {
             int statusCode = response.getStatusLine().getStatusCode();
             HttpEntity entity = response.getEntity();
             return consume.apply(statusCode, entity.getContent());
@@ -56,6 +59,10 @@ public class HttpExecutor {
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, apiKey));
 
-        return HttpClientBuilder.create().setDefaultCredentialsProvider(credentialsProvider).build();
-    }
+        return HttpClients
+                .custom()
+                .setDefaultCredentialsProvider(credentialsProvider)
+                .setDefaultRequestConfig(RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build())
+                .build();
+     }
 }
